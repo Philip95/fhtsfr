fe899d95c21f11ff57200a46e02766286c12d12c
Philip95 <philip.strobach@gmail.com>
2023-03-16 16:16:40 +0100

    Changed to maven

---
+++ sfr/sfr/src/main/java/com/example/sfr/SfrApplication.java
@@ -0,0 +1,13 @@
+package com.example.sfr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfrApplication.class, args);
+	}
+
+}
--- sfr/sfr/src/test/java/com/example/sfr/SfrApplicationTests.java
+++ sfr/sfr/src/test/java/com/example/sfr/SfrApplicationTests.java
@@ -1,4 +1,4 @@
-package com.sfr.sfr;
+package com.example.sfr;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
