8eba13f5722a0ddc226dbac43122cab062f55d63
Philip95 <philip.strobach@gmail.com>
2023-03-30 12:43:58 +0200

    Backend Methods

--- sfr/sfr/src/main/java/com/sfr/sfr/recipe/repository/RecipeRepository.java
+++ sfr/sfr/src/main/java/com/sfr/sfr/recipe/repository/RecipeRepository.java
@@ -4,6 +4,9 @@ import com.sfr.sfr.recipe.model.RecipeModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RecipeRepository extends JpaRepository<RecipeModel, Integer> {
+    List<RecipeModel> findByName(String name);
 }
--- sfr/sfr/src/main/java/com/sfr/sfr/recipe/service/RecipeService.java
+++ sfr/sfr/src/main/java/com/sfr/sfr/recipe/service/RecipeService.java
@@ -1,12 +1,41 @@
 package com.sfr.sfr.recipe.service;
 
+import com.sfr.sfr.recipe.model.RecipeModel;
 import com.sfr.sfr.recipe.repository.RecipeRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
 @AllArgsConstructor
 @Service
 public class RecipeService {
 
     private final RecipeRepository recipeRepository;
+
+    public List<RecipeModel> findAll() {
+        return recipeRepository.findAll();
+    }
+
+    public Optional<RecipeModel> findById(int id) {
+        return recipeRepository.findById(id);
+    }
+
+    public RecipeModel save(RecipeModel recipeModel) {
+        return recipeRepository.save(recipeModel);
+    }
+
+    public void deleteById(int id) {
+        recipeRepository.deleteById(id);
+    }
+
+    public void deleteAll() {
+        recipeRepository.deleteAll();
+    }
+
+    public List<RecipeModel> findByName(String name) {
+        return recipeRepository.findByName(name);
+    }
 
 }
